79d8e4cc096efcb0e7a95f54f7dca3a843b89125
Anton-Barash <60425459+Anton-Barash@users.noreply.github.comm>
2024-06-17 13:47:32 +0800

    esywork

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Login from './elements/Log'
+import EasyWork from './elements/EasyWork'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path='*' element={<Login />}></Route>
+        <Route path='EasyWork' element={<EasyWork />}></Route>
       </Routes>
     </>
   )
